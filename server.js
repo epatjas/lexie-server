4c91180e34b08197a5028afb01f3cbd1de381b83
epatjas <elina.patjas@gmail.com>
2024-12-11 13:04:00 +0200

    preparing for hosting the server on render.com

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const OpenAI = require('openai')
 require('dotenv').config()
-const sdk = require('microsoft-cognitiveservices-speech-sdk');
 const fs = require('fs');
 
 const app = express()
